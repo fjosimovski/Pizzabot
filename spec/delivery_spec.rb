2d28808f25d689b55cddca14f18092eddf29ab6c
Filip <josimovski.filip@gmail.com>
2019-06-17 14:14:08 +0200

    Specs for parsing grid size from command

--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -5,4 +5,17 @@ describe "Delivery" do
     delivery = Delivery.new("5x5 (1, 3) (4, 4), (0, 2)")
     expect(delivery.command).to eq("5x5 (1, 3) (4, 4), (0, 2)")
   end
+
+  context "command for parsing grid" do
+    it "succeed with grid in command" do
+      delivery = Delivery.new("5x5 (1, 3) (4, 4), (0, 2)")
+      grid = delivery.get_grid(delivery.command)
+      expect(grid).to eq("5x5")
+    end
+
+    it "raises error without grid in command" do
+      expect { Delivery.new("(1, 3) (4, 4), (0, 2)") }.to raise_error(ArgumentError)
+    end
+  end
+
 end
