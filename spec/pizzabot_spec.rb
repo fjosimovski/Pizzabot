5887d6a2eb36208bfe3dc37aeb8b95f82ad331a4
Filip <josimovski.filip@gmail.com>
2019-06-17 15:16:20 +0200

    Fix pizzabot specs

--- spec/pizzabot_spec.rb
+++ spec/pizzabot_spec.rb
@@ -2,12 +2,12 @@ require './src/pizzabot'
 
 describe "Pizzabot" do
   it "initialize pizzabot with grid size" do
-    pizzabot = Pizzabot.new("5x5")
-    expect(pizzabot.grid_size).to eq("5x5")
+    pizzabot = Pizzabot.new("5x5", ["(2,3)"])
+    expect(pizzabot.grid_size).to eq([5,5])
   end
 
   it "moves the pizzabot to position" do
-    pizzabot = Pizzabot.new("5x5")
+    pizzabot = Pizzabot.new("5x5", ["(2,3)"])
     path = pizzabot.move([0,0], [1,1])
     expect(path).to eq("WND")
   end
