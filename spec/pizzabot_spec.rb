93d8f8468fc1bcc7d1107a9b437494fe1c754fa4
Filip <josimovski.filip@gmail.com>
2019-06-17 13:49:43 +0200

    Add specs for moving pizzabot to position

--- spec/pizzabot_spec.rb
+++ spec/pizzabot_spec.rb
@@ -2,7 +2,13 @@ require './src/pizzabot'
 
 describe "Pizzabot" do
   it "initialize pizzabot with grid size" do
-  	pizzabot = Pizzabot.new("5x5")
+    pizzabot = Pizzabot.new("5x5")
     expect(pizzabot.grid_size).to eq("5x5")
+  end
+
+  it "moves the pizzabot to position" do
+    pizzabot = Pizzabot.new("5x5")
+    path = pizzabot.move([0,0], [1,1])
+    expect(path).to eq("WND")
   end
 end
