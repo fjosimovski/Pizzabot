9eae3f87c6c8f0da1d8985aa78ddf926e384035e
Filip <josimovski.filip@gmail.com>
2019-06-17 16:17:12 +0200

    Skip out of bounds delivery for pizzabot

--- src/pizzabot.rb
+++ src/pizzabot.rb
@@ -1,11 +1,24 @@
 #!/usr/bin/env ruby
 class Pizzabot
-  def initialize(grid_size)
+  def initialize(grid_size, drop_points)
     @grid_size = grid_size
+    @drop_points = drop_points
   end
 
   def grid_size
-    @grid_size
+    @grid_size.split("x").map(&:to_i)
+  end
+
+  def action(drop_points)
+    current_position = [0, 0]
+    path_string = ""
+    drop_points.each do |drop_point|
+      required_position = drop_point.gsub(/\(|\)/, '(' => '', ')' => '').split(',').map(&:to_i)
+      next if grid_size.zip(required_position).map{ |a,b| true if b > a}.include? true
+      path_string += move(current_position, required_position)
+      current_position = required_position
+    end
+    return path_string.to_s
   end
 
   def move(current_position, required_position)
