b88af799349e9c0d018ac8fc6129c8e44ca31cad
Filip <josimovski.filip@gmail.com>
2019-06-17 14:03:49 +0200

    Add command parser to find grid size and points for delivery

--- src/delivery.rb
+++ src/delivery.rb
@@ -1,11 +1,27 @@
 class Delivery
   # the command path is following "5x5 (1, 3) (4, 4), (0, 2)"
   def initialize(command)
-    @command = command  
+    @command = command 
+    parse_command(command)
   end
 
   def command
     @command
   end
 
+  def parse_command(command)
+    @grid_size = get_grid(command)
+    @drop_points = get_drop_points(command)
+  end
+
+  def get_grid(command)
+    grid = command.match(/\d+x\d+/)
+    raise ArgumentError, 'One grid size is required in format ex. 4x4' if grid.nil?
+    grid[0]
+  end
+
+  def get_drop_points(command)
+    # match drop points in command that are in brackets. Ex: (2,3) or (3, 4)
+    points = command.scan(/\(\s*\d+,\s*\d+\s*\)/)
+  end
 end
